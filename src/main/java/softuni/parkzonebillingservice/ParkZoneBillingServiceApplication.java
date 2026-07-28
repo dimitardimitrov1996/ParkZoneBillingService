f0a182ce6656d8862cc8419d93cb0e8abdd408a2
Dimitar Dimitrov <dimitrov.dimitar1996@gmail.com>
2026-07-28 21:34:33 +0300

    chore: initialize ParkZoneBillingService with Maven setup and basic application structure

---
+++ src/main/java/softuni/parkzonebillingservice/ParkZoneBillingServiceApplication.java
@@ -0,0 +1,13 @@
+package softuni.parkzonebillingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkZoneBillingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParkZoneBillingServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/softuni/parkzonebillingservice/ParkZoneBillingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package softuni.parkzonebillingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkZoneBillingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
