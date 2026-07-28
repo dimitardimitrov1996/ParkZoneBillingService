8f701c838a64becd480678abea00ccdc1e054055
Dimitar Dimitrov <dimitrov.dimitar1996@gmail.com>
2026-07-28 22:10:35 +0300

    feat: add InvoiceMapper for mapping Invoice entities to InvoiceResponse DTOs

---
+++ src/main/java/softuni/mapper/invoice/InvoiceMapper.java
@@ -0,0 +1,23 @@
+package softuni.mapper.invoice;
+
+import org.springframework.stereotype.Component;
+import softuni.parkzonebillingservice.model.dto.invoice.InvoiceResponse;
+import softuni.parkzonebillingservice.model.entity.Invoice;
+
+@Component
+public class InvoiceMapper {
+
+    public InvoiceResponse mapToResponse(Invoice invoice) {
+        return InvoiceResponse.builder()
+                .id(invoice.getId())
+                .reservationId(invoice.getReservationId())
+                .userId(invoice.getUserId())
+                .amount(invoice.getAmount())
+                .currency(invoice.getCurrency())
+                .status(invoice.getStatus())
+                .createdOn(invoice.getCreatedOn())
+                .paidOn(invoice.getPaidOn())
+                .cancelledOn(invoice.getCancelledOn())
+                .build();
+    }
+}
