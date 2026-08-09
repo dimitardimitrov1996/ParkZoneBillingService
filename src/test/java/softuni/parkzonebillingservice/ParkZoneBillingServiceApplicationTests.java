9cffd4f919815821b93832e0e5802f9b39079ebf
Dimitar Dimitrov <dimitrov.dimitar1996@gmail.com>
2026-08-09 21:45:16 +0300

    feat: enable test profile for ParkZoneBillingService application tests

--- src/test/java/softuni/parkzonebillingservice/ParkZoneBillingServiceApplicationTests.java
+++ src/test/java/softuni/parkzonebillingservice/ParkZoneBillingServiceApplicationTests.java
@@ -2,7 +2,9 @@ package softuni.parkzonebillingservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class ParkZoneBillingServiceApplicationTests {
 
